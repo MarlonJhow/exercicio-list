9d518914769ff26a6431dcc19b91d944c104412f
MarlonJhow <jhow.mjy@gmail.com>
2026-08-24 12:57:08 -0300

    Add files via upload

---
+++ src/main/java/CalculadoraService.java
@@ -0,0 +1,9 @@
+public class CalculadoraService {
+
+    public static int calcular(int number1, int number2, String operator) {
+        // Implemente a operação utilizando o operador informado.
+        // Operadores suportados: +, -, *, /
+        // Retorne o resultado da operação.
+        return 0;
+    }
+}
